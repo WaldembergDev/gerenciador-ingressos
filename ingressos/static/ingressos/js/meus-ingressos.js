64bfdbc5c58bb270262f67b135607cb9fc593cd8
Waldemberg <waldemberg.pereira@outlook.com>
2026-07-31 20:38:33 -0300

    fix(ingressos): corrigir modal meus ingressos

--- ingressos/static/ingressos/js/meus-ingressos.js
+++ ingressos/static/ingressos/js/meus-ingressos.js
@@ -17,52 +17,49 @@ document.addEventListener('DOMContentLoaded', function(){
     }
     // declarando as variáveis
     let botoes = document.querySelectorAll('.btn-ingresso');
-    let modal = document.getElementById('modal');
-    let modalBootstrap = new bootstrap.Modal(modal);
+    let modal = document.getElementById('modalDetalhes');
     const csrfToken = getCookie('XSRF-TOKEN');
+    
+    modal.addEventListener('show.bs.modal', function(event){
+        botao = event.relatedTarget;
+        let idHistorico = botao.dataset.id;
+        let urlIngresso = `/ingressos/json-detalhes-compra/${idHistorico}`
+        // realizando requisição para a api do django
+        fetch(urlIngresso, {
+            headers: {
+                'X-CSRF-TOKEN': csrfToken,
+                'Content-Type': 'application/json'
+            }
+        }).then(response =>{
+            if (response.ok){
+                return response.json();
+            }else{
+                alert('Erro ao realizar requisição');
+            }
+        }).then(json =>{
+            // obtendo as variaveis do modal
+            let titulo = document.getElementById('tituloId');
+            let local = document.getElementById('localId');
+            let dataCompraId = document.getElementById('dataCompraId');
+            let valorPagoId = document.getElementById('valorPagoId');
+            let quantidadeId = document.getElementById('quantidadeId');
 
-    botoes.forEach(botao => {
-        botao.addEventListener('click', function(){
-            let idHistorico = botao.dataset.id;
-            let urlIngresso = `/ingressos/json-detalhes-compra/${idHistorico}`
-            // realizando requisição para a api do django
-            fetch(urlIngresso, {
-                headers: {
-                    'X-CSRF-TOKEN': csrfToken,
-                    'Content-Type': 'application/json'
-                }
-            }).then(response =>{
-                if (response.ok){
-                    return response.json();
-                }else{
-                    alert('Erro ao realizar requisição');
-                }
-            }).then(json =>{
-                // obtendo as variaveis do modal
-                let titulo = document.getElementById('tituloId');
-                let local = document.getElementById('localId');
-                let dataCompraId = document.getElementById('dataCompraId');
-                let valorPagoId = document.getElementById('valorPagoId');
-                let quantidadeId = document.getElementById('quantidadeId');
-
-                /* formatando a data */
-                const data = new Date(json.data_compra);
-                const dataFormatada = data.toLocaleString('pt-BR', {
-                    day: '2-digit', month: '2-digit', year: 'numeric',
-                    hour: '2-digit', minute: '2-digit'
-                });
+            /* formatando a data */
+            const data = new Date(json.data_compra);
+            const dataFormatada = data.toLocaleString('pt-BR', {
+                day: '2-digit', month: '2-digit', year: 'numeric',
+                hour: '2-digit', minute: '2-digit'
+            });
 
 
-                // salvando os dados no modal
-                titulo.textContent = json.titulo;
-                local.textContent = json.local;
-                dataCompraId.textContent = dataFormatada;
-                valorPagoId.textContent = json.valor_pago;
-                quantidadeId.textContent = json.quantidade;
-                modalBootstrap.show();
-            }).catch(erro =>{
-                console.log(erro)
-            })              
-        })
+            // salvando os dados no modal
+            titulo.textContent = json.titulo;
+            local.textContent = json.local;
+            dataCompraId.textContent = dataFormatada;
+            valorPagoId.textContent = json.valor_pago;
+            quantidadeId.textContent = json.quantidade;
+        }).catch(erro =>{
+            console.log(erro)
+        })              
     })
 });
