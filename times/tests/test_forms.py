b44fe30c8031856b839191175d0864d042c8e7a3
Waldemberg <waldemberg.pereira@outlook.com>
2026-08-30 22:51:57 -0300

    feat(times): adicionar testes de forms

---
+++ times/tests/test_forms.py
@@ -0,0 +1,37 @@
+import pytest
+from times.forms import TimeForm
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+
+@pytest.mark.django_db
+def test_time_form_valido():
+    conteudo_imagem = (
+            b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff'
+            b'\x00\x00\x00\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00'
+            b'\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b'
+        )
+    
+    escudo_fake = SimpleUploadedFile(
+        name = 'teste.gif',
+        content = conteudo_imagem,
+        content_type = 'image/gif'
+    )
+
+    dados = {
+        'nome': 'Time_Teste',
+    }
+    arquivos = {
+        'escudo': escudo_fake
+    }
+
+    time = TimeForm(data=dados, files=arquivos)
+
+    assert time.is_valid()
+
+@pytest.mark.django_db
+def test_time_form_invalido():
+    dados = {'nome': 'Time_Teste'}
+
+    time = TimeForm(data=dados)
+
+    assert not time.is_valid()
