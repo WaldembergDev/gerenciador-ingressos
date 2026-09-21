1cfa2d48fedca84df9e5f31b2340e3ab8c8e6855
Waldemberg <waldemberg.pereira@outlook.com>
2026-09-20 23:17:21 -0300

    feat(core): testar view login

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -43,3 +43,60 @@ def test_home(client, ingresso_comum):
     assert response.status_code == 200
 
     assert ingresso_comum in response.context['ingressos']
+
+
+@pytest.mark.django_db
+def test_login(client, cliente_comum):
+    url = reverse('core_login')
+
+    session = client.session
+    session['acesso_geral'] = 'teste@123'
+    session.save()
+
+    formulario_login = {
+        'username': 'teste@gmail.com',
+        'password': '@1234@456@'
+    }
+
+    response = client.post(url, data=formulario_login)
+
+    assert response.status_code == 302
+
+    assert response['Location'] == reverse('home')
+
+
+@pytest.mark.django_db
+def test_login_senha_errada(client, cliente_comum):
+    url = reverse('core_login')
+
+    session = client.session
+    session['acesso_geral'] = 'teste@123'
+    session.save()
+
+    formulario_login = {
+        'username': 'teste@gmail.com',
+        'password': '123456'
+    }
+
+    response = client.post(url, data=formulario_login)
+
+    form_errors = response.context['form'].errors
+
+    assert 'password' in form_errors or '__all__' in form_errors
+
+@pytest.mark.django_db
+def test_login_autenticado(client, usuario_comum):
+    url = reverse('core_login')
+
+    session = client.session
+    session['acesso_geral'] = 'teste@123'
+    session.save()
+
+    client.force_login(usuario_comum)
+
+    response = client.get(url)
+
+    assert response.status_code == 302
+
+    assert response['Location'] == reverse('home')
+    
