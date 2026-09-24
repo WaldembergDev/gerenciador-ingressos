b6f0a251864ea85819910c0ca2bc2ccc2753a56d
Waldemberg <waldemberg.pereira@outlook.com>
2026-09-23 23:02:52 -0300

    chore: adicionar default variáveis de ambiente ligadas a e-mail

--- maraca_ingresso/settings/base.py
+++ maraca_ingresso/settings/base.py
@@ -136,8 +136,8 @@ LOGOUT_REDIRECT_URL = "/core/login"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = config("EMAIL_HOST", default='')
-EMAIL_PORT = config("EMAIL_PORT", cast=int, default='')
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default='')
+EMAIL_PORT = config("EMAIL_PORT", cast=int, default=10)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=True)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER", default='')
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default='')
 EMAIL_FROM_ADDRESS = config("EMAIL_FROM_ADDRESS", default='')
